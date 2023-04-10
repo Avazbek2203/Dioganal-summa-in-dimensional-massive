968f2b2823602dfb5ac35db04c13322b68e91524
Avazbek2203 <128214329+Avazbek2203@users.noreply.github.com>
2023-04-10 12:15:59 +0500

    Add project files.

---
+++ Revision 1.2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Revision_1._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            NewArray();
+
+        }
+        public static void NewArray()
+        {
+            Random random = new Random();
+
+            Console.Write("Enter row length: ");
+            int firstInput = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter column length: ");
+            int secondInput = int.Parse(Console.ReadLine());
+
+            int[][] array = new int[firstInput][];
+            for (int i = 0; i < firstInput; i++)
+            {
+                array[i] = new int[secondInput];
+                for (int j = 0; j < secondInput; j++)
+                {
+                    array[i][j] = random.Next(1, 5);
+                    Console.Write(array[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            int diagonalSum = 0;
+            for (int i = 0; i < firstInput && i < secondInput; i++)
+            {
+                diagonalSum += array[i][i];
+            }
+            Console.WriteLine("Diagonal sum: " + diagonalSum);
+
+            int rowSum = 0;
+            for (int i = 0; i < firstInput; i++)
+            {
+                rowSum += array[array[i].Length - 1][i];
+            }
+            Console.WriteLine($"Row sum : {rowSum}");
+            if (diagonalSum > rowSum)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
